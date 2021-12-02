693587f98cdccfaf42bd6be7c1f9fc7fbf9f8e52
Evyatar Hen <evyatar037@gmail.com>
2021-12-02 21:49:22 +0200

    add new files
    update

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-function SearchBox(props){
-    return (
-       <div className="col cols-sm-4">
-           <input
-             className="form-control"
-             value={props.value}
-             onChange={ (event) => props.setSearchValue(event.target.value)}
-             placeholder="Type to search..."
-           />
-       </div>
-    );
-}
+const SearchBox = (props) => {
+	return (
+		<div className='col col-sm-4'>
+			<input
+				className='form-control'
+				value={props.value}
+				onChange={(event) => props.setSearchValue(event.target.value)}
+				placeholder='Type to search...'
+			></input>
+		</div>
+	);
+};
 
 export default SearchBox;
